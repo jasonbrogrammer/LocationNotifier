845900506eea68eb7a20b51161c18ac11fc85176
jasonbrogrammer <jason.n.chang@gmail.com>
2013-06-20 19:13:43 -0700

    Initial Commit

---
+++ LocationNotifier/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  LocationNotifier
+//
+//  Created by Jason Chang on 6/20/13.
+//  Copyright (c) 2013 Jason Chang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ LocationNotifier/MasterViewController.h
@@ -0,0 +1,17 @@
+//
+//  MasterViewController.h
+//  LocationNotifier
+//
+//  Created by Jason Chang on 6/20/13.
+//  Copyright (c) 2013 Jason Chang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class DetailViewController;
+
+@interface MasterViewController : UITableViewController
+
+@property (strong, nonatomic) DetailViewController *detailViewController;
+
+@end
